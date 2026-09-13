bdfd22e8f6df1eefb8462bff19eb1c82dfefc08a
Saurav Sharma <sauravsharma.098098@gmail.com>
2026-09-13 12:38:55 +0530

    Add files via upload

--- Program_11/program.py
+++ Program_11/program.py
@@ -1,1 +1,17 @@
+import cv2
+import numpy as np
 
+img = cv2.imread("input.jpg")
+
+smooth = cv2.GaussianBlur(img, (5, 5), 0)
+
+kernel = np.array([
+    [0, -1,  0],
+    [-1, 5, -1],
+    [0, -1,  0]
+], dtype=np.float32)
+
+sharp = cv2.filter2D(img, -1, kernel)
+
+cv2.imwrite("output_smooth.png", smooth)
+cv2.imwrite("output_sharp.png", sharp)

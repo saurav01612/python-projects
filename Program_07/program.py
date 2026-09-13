76656ffc8273da18cee09e80c4623b83437128ad
Saurav Sharma <sauravsharma.098098@gmail.com>
2026-09-13 14:32:47 +0530

    Delete Program_07 directory

--- Program_07/program.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-img = cv2.imread("input.jpg")
-
-# 5x5 is an odd-sized kernel that gives effective smoothing while retaining
-# more detail than a very large Gaussian kernel.
-result = cv2.GaussianBlur(img, (5, 5), 0)
-
-cv2.imwrite("output.png", result)

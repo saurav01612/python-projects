6abde4e2309bca0853f04c387395c062277a6e19
Saurav Sharma <sauravsharma.098098@gmail.com>
2026-09-13 15:08:27 +0530

    Add files via upload

--- 06_Mean_Filter/program.py
+++ 06_Mean_Filter/program.py
@@ -1,1 +1,13 @@
+import cv2
+import numpy as np
 
+img = cv2.imread("input.jpg")
+
+# Tested with smaller kernels; final output uses the larger 7x7 kernel.
+kernel_small = np.ones((3, 3), np.float32) / 9
+result_small = cv2.filter2D(img, -1, kernel_small)
+
+kernel_large = np.ones((7, 7), np.float32) / 49
+result_large = cv2.filter2D(img, -1, kernel_large)
+
+cv2.imwrite("output.png", result_large)

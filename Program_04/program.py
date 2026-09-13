36ce0d89087eada4ab7b51fc8d296af2624f1389
Saurav Sharma <sauravsharma.098098@gmail.com>
2026-09-13 12:25:28 +0530

    Add files via upload

--- Program_04/program.py
+++ Program_04/program.py
@@ -1,1 +1,18 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
+img = cv2.imread("input.jpg", cv2.IMREAD_GRAYSCALE)
+
+hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+highest_frequency_intensity = int(np.argmax(hist))
+
+print("Intensity with highest frequency:", highest_frequency_intensity)
+
+plt.figure()
+plt.plot(hist)
+plt.xlabel("Intensity")
+plt.ylabel("Frequency")
+plt.title("Image Histogram")
+plt.savefig("output.png", bbox_inches="tight")
+plt.close()
